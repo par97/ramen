977204e4fcc2bfd601be9a21093264810c24c3a5
jacklu <jilu@redhat.com>
2024-03-25 18:23:42 +0800

    add enabel dr basic functions
    Signed-off-by: jacklu <jilu@redhat.com>

--- e2e/dractions/actions.go
+++ e2e/dractions/actions.go
@@ -4,13 +4,16 @@ import (
 	"context"
 	"fmt"
 
+	ramen "github.com/ramendr/ramen/api/v1alpha1"
 	"github.com/ramendr/ramen/e2e/deployers"
 	"github.com/ramendr/ramen/e2e/util"
 	"github.com/ramendr/ramen/e2e/workloads"
+	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/dynamic"
 	clusterv1beta1 "open-cluster-management.io/api/cluster/v1beta1"
 )
 
@@ -19,6 +22,62 @@ type DRActions struct {
 }
 
 const OCM_SCHEDULING_DISABLE = "cluster.open-cluster-management.io/experimental-scheduling-disable"
+
+func getPlacement(client *dynamic.DynamicClient, namespace, name string) (*clusterv1beta1.Placement, error) {
+
+	resource := schema.GroupVersionResource{Group: "cluster.open-cluster-management.io", Version: "v1beta1", Resource: "placements"}
+	unstr, err := client.Resource(resource).Namespace(namespace).Get(context.Background(), name, metav1.GetOptions{})
+	if err != nil {
+		fmt.Printf("err: %v\n", err)
+		return nil, fmt.Errorf("could not get placement CR")
+	}
+
+	placement := clusterv1beta1.Placement{}
+	err = runtime.DefaultUnstructuredConverter.FromUnstructured(unstr.UnstructuredContent(), &placement)
+	if err != nil {
+		fmt.Println(err)
+		return nil, fmt.Errorf("could not FromUnstructured in func getPlacment")
+	}
+
+	return &placement, nil
+}
+
+func updatePlacement(client *dynamic.DynamicClient, placement *clusterv1beta1.Placement) error {
+
+	tempMap, err := runtime.DefaultUnstructuredConverter.ToUnstructured(placement)
+	if err != nil {
+		fmt.Println(err)
+		return fmt.Errorf("could not ToUnstructured")
+	}
+
+	unstr := &unstructured.Unstructured{Object: tempMap}
+	resource := schema.GroupVersionResource{Group: "cluster.open-cluster-management.io", Version: "v1beta1", Resource: "placements"}
+	_, err = client.Resource(resource).Namespace(placement.GetNamespace()).Update(context.TODO(), unstr, metav1.UpdateOptions{})
+	if err != nil {
+		fmt.Println(err)
+		return fmt.Errorf("could not update placment")
+	}
+
+	return nil
+}
+
+func getPlacementDecision(client *dynamic.DynamicClient, namespace, name string) (*clusterv1beta1.PlacementDecision, error) {
+	resource := schema.GroupVersionResource{Group: "cluster.open-cluster-management.io", Version: "v1beta1", Resource: "placementdecisions"}
+	unstr, err := client.Resource(resource).Namespace(namespace).Get(context.Background(), name, metav1.GetOptions{})
+	if err != nil {
+		fmt.Printf("err: %v\n", err)
+		return nil, fmt.Errorf("could not get placementDecision CR")
+	}
+
+	placementDecision := clusterv1beta1.PlacementDecision{}
+	err = runtime.DefaultUnstructuredConverter.FromUnstructured(unstr.UnstructuredContent(), &placementDecision)
+	if err != nil {
+		fmt.Println(err)
+		return nil, fmt.Errorf("could not FromUnstructured in func getPlacementDecision")
+	}
+
+	return &placementDecision, nil
+}
 
 func (r DRActions) EnableProtection(w workloads.Workload, d deployers.Deployer) error {
 	// If AppSet/Subscription, find Placement
@@ -31,36 +90,95 @@ func (r DRActions) EnableProtection(w workloads.Workload, d deployers.Deployer) 
 
 	_, ok := d.(deployers.Subscription)
 	if ok {
+
+		name := "deployment-rbd"
+		namespace := "deployment-rbd"
+		drPolicyName := "dr-policy"
+		pvcLabel := "busybox"
+		placementName := "placement"
+		placementKind := "placement"
 		client := r.Ctx.HubDynamicClient()
 
-		resource := schema.GroupVersionResource{Group: "cluster.open-cluster-management.io", Version: "v1beta1", Resource: "placements"}
-		unstr, err := client.Resource(resource).Namespace("deployment-rbd").Get(context.TODO(), "placement", metav1.GetOptions{})
+		placement, err := getPlacement(client, namespace, placementName)
 		if err != nil {
-			fmt.Printf("err: %v\n", err)
-			return fmt.Errorf("could not get placement")
-		}
-
-		placement := clusterv1beta1.Placement{}
-		err = runtime.DefaultUnstructuredConverter.FromUnstructured(unstr.UnstructuredContent(), &placement)
-		if err != nil {
-			fmt.Println(err)
-			return fmt.Errorf("could not FromUnstructured")
+			return err
 		}
 
 		placement.Annotations[OCM_SCHEDULING_DISABLE] = "true"
 
-		tempMap, err := runtime.DefaultUnstructuredConverter.ToUnstructured(&placement)
+		err = updatePlacement(client, placement)
+		if err != nil {
+			return err
+		}
+
+		// L1
+		placement, err = getPlacement(client, namespace, placementName)
+		if err != nil {
+			return err
+		}
+		placementDecisionName := ""
+		for _, cond := range placement.Status.Conditions {
+			if cond.Type == "PlacementSatisfied" && cond.Status == "True" {
+				placementDecisionName = placement.Status.DecisionGroups[0].Decisions[0]
+				// kubectl.get(
+				// 	"placementdecisions",
+				// 	f"--selector=cluster.open-cluster-management.io/placement={placement_name}",
+				// 	f"--namespace={config['namespace']}",
+				// 	"--output=jsonpath={.items[0].status.decisions[0].clusterName}",
+				// 	context=env["hub"],
+			}
+		}
+		if placementDecisionName == "" {
+			fmt.Println("can not find placement decision")
+			// if not timeout, go to L1
+			// else return error
+		}
+
+		placementDecision, err := getPlacementDecision(client, namespace, placementDecisionName)
+		if err != nil {
+			return err
+		}
+
+		clusterName := placementDecision.Status.Decisions[0].ClusterName
+		fmt.Printf("clusterName: %v\n", clusterName)
+
+		drpc := &ramen.DRPlacementControl{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      name + "-drpc",
+				Namespace: namespace,
+				Labels:    map[string]string{"app": name},
+			},
+			Spec: ramen.DRPlacementControlSpec{
+				PreferredCluster: clusterName,
+				DRPolicyRef: v1.ObjectReference{
+					Name: drPolicyName,
+				},
+				PlacementRef: v1.ObjectReference{
+					Kind: placementKind,
+					Name: placementName,
+				},
+				PVCSelector: metav1.LabelSelector{
+					MatchLabels: map[string]string{"appname": pvcLabel},
+				},
+			},
+		}
+
+		tempMap, err := runtime.DefaultUnstructuredConverter.ToUnstructured(drpc)
 		if err != nil {
 			fmt.Println(err)
 			return fmt.Errorf("could not ToUnstructured")
 		}
-		unstr = &unstructured.Unstructured{Object: tempMap}
-		_, err = client.Resource(resource).Namespace("deployment-rbd").Update(context.TODO(), unstr, metav1.UpdateOptions{})
+
+		unstr := &unstructured.Unstructured{Object: tempMap}
+		resource := schema.GroupVersionResource{Group: "cluster.open-cluster-management.io", Version: "v1beta1", Resource: "drplacementcontrols"}
+		_, err = client.Resource(resource).Namespace(drpc.GetNamespace()).Update(context.TODO(), unstr, metav1.UpdateOptions{})
 		if err != nil {
 			fmt.Println(err)
-			return fmt.Errorf("could not update placment")
+			return fmt.Errorf("could not update drplacementcontrol")
 		}
-		r.Ctx.Log.Info("updated placement annotation")
+
+		return nil
+
 	}
 
 	return nil
--- e2e/go.mod
+++ e2e/go.mod
@@ -4,15 +4,20 @@ go 1.21.7
 
 require (
 	github.com/go-logr/logr v1.4.1
+	github.com/ramendr/ramen/api v0.0.0-00010101000000-000000000000
 	github.com/rook/rook/pkg/apis v0.0.0-20240123174716-ab31635d9b98
+	k8s.io/api v0.29.3
 	k8s.io/apimachinery v0.29.3
 )
 
 require (
+	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cenkalti/backoff/v3 v3.2.2 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/containernetworking/cni v1.1.2 // indirect
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/emicklei/go-restful/v3 v3.11.3 // indirect
+	github.com/evanphx/json-patch/v5 v5.8.0 // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
 	github.com/go-jose/go-jose/v3 v3.0.1 // indirect
 	github.com/go-logr/zapr v1.3.0 // indirect
@@ -20,8 +25,10 @@ require (
 	github.com/go-openapi/jsonreference v0.20.5 // indirect
 	github.com/go-openapi/swag v0.22.10 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/google/gnostic-models v0.6.8 // indirect
+	github.com/google/go-cmp v0.6.0 // indirect
 	github.com/google/gofuzz v1.2.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
@@ -44,6 +51,7 @@ require (
 	github.com/libopenstorage/secrets v0.0.0-20231011182615-5f4b25ceede1 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/matttproud/golang_protobuf_extensions/v2 v2.0.0 // indirect
 	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
@@ -52,6 +60,10 @@ require (
 	github.com/openshift/api v0.0.0-20231204192004-bfea29e5e6c4 // indirect
 	github.com/pelletier/go-toml/v2 v2.1.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/prometheus/client_golang v1.18.0 // indirect
+	github.com/prometheus/client_model v0.5.0 // indirect
+	github.com/prometheus/common v0.45.0 // indirect
+	github.com/prometheus/procfs v0.12.0 // indirect
 	github.com/ryanuber/go-glob v1.0.0 // indirect
 	github.com/sagikazarmark/locafero v0.4.0 // indirect
 	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
@@ -60,6 +72,7 @@ require (
 	github.com/spf13/afero v1.11.0 // indirect
 	github.com/spf13/cast v1.6.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/stolostron/multicloud-operators-placementrule v1.2.4-1-20220311-8eedb3f.0.20230828200208-cd3c119a7fa0 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
 	golang.org/x/crypto v0.21.0 // indirect
 	golang.org/x/exp v0.0.0-20231127185646-65229373498e // indirect
@@ -69,13 +82,15 @@ require (
 	golang.org/x/term v0.18.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	golang.org/x/time v0.5.0 // indirect
+	gomodules.xyz/jsonpatch/v2 v2.4.0 // indirect
 	google.golang.org/appengine v1.6.8 // indirect
 	google.golang.org/protobuf v1.33.0 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
-	k8s.io/api v0.29.3 // indirect
+	k8s.io/apiextensions-apiserver v0.29.0 // indirect
+	k8s.io/component-base v0.29.1 // indirect
 	k8s.io/klog/v2 v2.120.1 // indirect
 	k8s.io/kube-openapi v0.0.0-20240228011516-70dd3763d340 // indirect
 	k8s.io/utils v0.0.0-20240102154912-e7106e64919e // indirect
@@ -95,6 +110,7 @@ require (
 
 replace (
 	github.com/portworx/sched-ops => github.com/portworx/sched-ops v0.20.4-openstorage-rc3 // required by rook v1.7
+	github.com/ramendr/ramen/api => ../api
 	k8s.io/api => k8s.io/api v0.28.3
 	k8s.io/client-go => k8s.io/client-go v0.28.3
 )
